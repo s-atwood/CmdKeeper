d7e4def84031703072279dfaeaf21d00a4466fb5
Shawn Atwood <sdatwood4@gmail.com>
2024-08-17 02:38:53 -0400

    create edit_command function; separate tag validation logic

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import argparse
 import json
 import os
+import subprocess
 import sys
+import tempfile
 from pathlib import Path
 
 import pyperclip
 
-STOARGE_FILE = "commands.json"
+STORAGE_FILE = "commands.json"
 commands = {}
 
 
-if not os.path.exists(STOARGE_FILE):
-    with open(STOARGE_FILE, "w") as f:
+if not os.path.exists(STORAGE_FILE):
+    with open(STORAGE_FILE, "w") as f:
         json.dump({}, f, indent=4)
 else:
-    with open(STOARGE_FILE, "r") as f:
+    with open(STORAGE_FILE, "r") as f:
         commands = json.load(f)
 
 
@@ -23,26 +25,57 @@ def add_command(tag, command):
         commands[tag].append(command)
     else:
         commands[tag] = [command]
-    with open(STOARGE_FILE, "w") as f:
+    with open(STORAGE_FILE, "w") as f:
         json.dump(commands, f, indent=4)
 
 
-def copy_command(tag=None):
+def validate_tag(action, tag):
     if tag is None:
-        print("Select a tag to copy from:")
+        print(f"Select a tag to {action} from:")
         tags = commands.keys()
         for i, t in enumerate(tags):
             print(f"{i+1}: {t}")
-        cmd_selection = int(input("Which command to copy?: "))
+        cmd_selection = int(input(f"Which tag to {action} from?: "))
         tag = list(tags)[cmd_selection - 1]
 
-    if tag in commands:
+    if tag not in commands:
+        print("Tag not found")
+        return None, None
+    else:
         for i in range(len(commands[tag])):
             print(f"{i+1}: {commands[tag][i]}")
-        selection = int(input("Which command to copy?: "))
+        selection = int(input(f"Which command to {action}?: "))
+        return selection, tag
+
+
+def copy_command(action, tag):
+    selection, tag = validate_tag(action, tag)
+    
+    if selection is not None:
         pyperclip.copy(commands[tag][selection - 1])
-    else:
-        print("Tag not found")
+        print(f"Command copied to clipboard: {commands[tag][selection - 1]}")
+
+
+def edit_command(action, tag):
+
+    selection, tag = validate_tag(action, tag)
+
+    if selection is not None:
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False) as temp_file:
+            temp_file.write(commands[tag][selection - 1])
+            temp_filepath = temp_file.name
+
+        subprocess.run(["nvim", temp_filepath])
+
+        with open(temp_filepath, "r") as temp_file:
+            updated_command = temp_file.read().strip()
+
+        commands[tag][selection - 1] = updated_command
+
+        with open(STORAGE_FILE, "w") as f:
+            json.dump(commands, f, indent=4)
+
+        print(f"Command updated for tag '{tag}'.")
 
 
 def list_commands():
@@ -54,13 +87,14 @@ def list_commands():
 
 def main():
     parser = argparse.ArgumentParser(
-        description="A simple CLI tool to store terminal commands."
+        description="A simple CLI tool to store terminal commands.",
+        formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=40),
     )
 
     group = parser.add_mutually_exclusive_group()
 
     group.add_argument(
-        "-a", "--add", nargs=2, metavar=("tag", "command"), help="Add a new command"
+        "-a", "--add", nargs=2, metavar=("[tag]", "[command]"), help="Add a new command"
     )
     group.add_argument(
         "-c",
@@ -70,6 +104,16 @@ def main():
         metavar="tag",
         help="Copy a command to clipboard",
     )
+
+    group.add_argument(
+        "-e",
+        "--edit",
+        nargs="?",
+        const="__NO_TAG__",
+        metavar="edit",
+        help="Edit a command",
+    )
+
     group.add_argument(
         "-l", "--list", action="store_true", help="List all tags and commands"
     )
@@ -85,9 +129,14 @@ def main():
         add_command(tag, command)
     elif args.copy is not None:
         if args.copy == "__NO_TAG__":
-            copy_command(None)
+            copy_command("copy", None)
         else:
-            copy_command(args.copy)
+            copy_command("copy", args.copy)
+    elif args.edit is not None:
+        if args.edit == "__NO_TAG__":
+            edit_command("edit", None)
+        else:
+            edit_command("edit", args.edit)
     elif args.list:
         list_commands()
     else:
